bac6c82d532a3d30389f3075fee61522f7721c75
Joey Frohlinger <jmafrohlinger@outlook.com>
2020-02-11 11:05:45 -0800

    a small script to help pave a new machine and install some standard applications

---
+++ new-computer.ps1
@@ -0,0 +1,20 @@
+# Remove Pre-Installed Garbage
+Get-AppxPackage king.com.CandyCrushFriends | Remove-AppxPackage  
+Get-AppxPackage king.com.FarmHeroesSaga | Remove-AppxPackage  
+Get-AppxPackage 7EE7776C.LinkedInforWindows | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage 7EE7776C.LinkedInforWindows | Remove-AppxPackage
+Get-AppxPackage 7EE7776C.LinkedInforWindows | Remove-AppxPackage
+
+
+
+# Chocolatey and Apps  
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install googlechrome -y
+choco install vscode -y
+choco install git -y
+choco install steam -y
+choco install discord -y
+choco install spotify -y
