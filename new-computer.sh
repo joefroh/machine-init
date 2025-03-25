9c0d5789e68b53f66299ace5d470aa267f59b61d
Joey Frohlinger <jmafrohlinger@outlook.com>
2025-03-24 22:31:19 -0700

    Adding shell script for my new macbook pro

---
+++ new-computer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# tiny bash script to install everything I need on a new macbook
+
+# Install Homebrew
+curl -sSLf -o homebrew-installer https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+bash homebrew-installer
+rm homebrew-installer
+echo '$PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"' >> ~/.bashrc
+
+echo Homebrew should be installed now. Installing packages...
+
+# Install Homebrew packages
+brew install \
+ git \
+ google-chrome \
+ iterm2 \
+ visual-studio-code \
+ spotify \
+ microsoft-outlook \
+ pyenv \
+
+# Setup Pyenv for zsh
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
+
+brew install openssl readline sqlite3 xz zlib tcl-tk@8 libb2
+
+# Install Python 3.10 and configure it as global
+pyenv install 3.10
+
+pyenv global 3.10
+
+# Refresh the shell, because apparently thats needed
+exec "$SHELL"
+
+# Update pip
+pip install --upgrade pip
+
